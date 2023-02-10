effa51a08088865745b8c2d2ddaf2ca3525dd30d
Dhananjay Pant <86868676+Dhananjay-12@users.noreply.github.com>
2023-02-10 23:37:04 +0530

    Responsive  SPA

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
